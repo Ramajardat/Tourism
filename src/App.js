b170655efc575d2ceca92b9800d101b9d814f3f8
Ramajardat <ramajardat6@gmail.com>
2022-11-06 07:48:50 -0800

    add Task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Toursinfo from "./components/Toursinfo";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/Tours-info/:id" element={<Toursinfo />}></Route>
       </Routes>
       <Footer />
     </div>
--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -9,7 +9,7 @@ function Tour (data){
             <img src={city.image} style={{ width:"100%" , height:"100%" }} alt="" width="700px"/>
             </div>  
             <p className="Tname p-3" style={{color:"green"}} >{city.name}</p>
-            <p className="Tname p-3" style={{color:"green"}} >{city.info}</p>
+            {/* <p className="Tname p-3" style={{color:"green"}} >{city.info}</p> */}
 
     </div>
     </>
